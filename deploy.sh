02476e695034e7390f747d3ee416ff5d3c71d0e7
Yanik Beaulieu <yanikbeaulieu@hotmail.com>
2022-04-09 02:37:07 -0400

    deploy node server

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt install -y nodejs
+node -v
+
+ip a
+
+
+
+mkdir servers
+cd servers
+
+sudo npm install -g pm2
+pm2 status
+sudo apt install git
+git clone https://github.com/WindriderQc/iGrow.git
+cd iGrow
+npm install
+npm run dev
